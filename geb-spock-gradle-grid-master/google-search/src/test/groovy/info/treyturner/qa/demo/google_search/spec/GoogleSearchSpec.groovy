2bc5cf1a414dea33527d6dec8873b784f41f79ed
Tianjiao Zhu <tzhu@MacBook-Pro-3.local>
2016-07-20 16:15:09 -0500

    initial commit

---
+++ geb-spock-gradle-grid-master/google-search/src/test/groovy/info/treyturner/qa/demo/google_search/spec/GoogleSearchSpec.groovy
@@ -0,0 +1,47 @@
+package info.treyturner.qa.demo.google_search.spec
+
+import info.treyturner.qa.demo.google_search.WebApplicationSpecification
+import info.treyturner.qa.demo.google_search.page.GoogleResultsPage
+import info.treyturner.qa.demo.google_search.page.GoogleSearchPage
+
+class GoogleSearchSpec extends WebApplicationSpecification {
+
+    def "should load the Google search page"() {
+
+        given: "the user navigates to the Google search page"
+        to GoogleSearchPage
+
+        expect: "the Google search page to be shown"
+        at GoogleSearchPage
+    }
+
+    def "should return search results when performing a search"() {
+
+        given: "the user is at the Google search page"
+        at GoogleSearchPage
+
+        when: "the user types 'The White House' into the search field"
+        searchBox = 'The White House'
+
+        and: "the user clicks the search button"
+        searchButton.click()
+
+        then: "the user is brought to the search results page"
+        at GoogleResultsPage
+
+        and: "some number of search results are returned"
+        results.size() > 1
+    }
+
+    def "should return whitehouse.gov as the first result for a search for 'The White House'"() {
+
+        given: "the user is at the former search result page"
+        at GoogleResultsPage
+
+        expect: "the first result to be for whitehouse.gov"
+        results[0].name.text() == "The White House | whitehouse.gov"
+        results[0].link.@href == "https://www.whitehouse.gov/"
+        results[0].description.text() == "Official White House site presents issue positions, news, Cabinet, " +
+                "appointments, offices and major speeches. Includes biography, video tour and photo essays."
+    }
+}
---
+++ geb-spock-gradle-grid-master/util/src/main/groovy/info/treyturner/qa/demo/util/WebDriver.groovy
@@ -0,0 +1,117 @@
+package info.treyturner.qa.demo.util
+
+import geb.js.JavascriptInterface
+import geb.navigator.Navigator
+import groovy.util.logging.Slf4j
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriverService
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
+
+@Slf4j
+class WebDriver {
+
+    static String gridUri = "http://your.grid.ip.address:4444/wd/hub";
+
+    static Closure configureDriver(String browserLocation, String browserType, String platform) {
+        Closure driver
+        switch (browserLocation) {
+            case 'local':
+                switch (browserType) {
+                    case 'firefox':
+                        driver = { new FirefoxDriver() }
+                        break
+                    case 'chrome':
+                        def chromeDriverVersion = "2.22"
+                        switch (platform) {
+                            case 'windows32':
+                            case 'windows64':
+                                System.setProperty("webdriver.chrome.driver", "../util/drivers/chrome/$chromeDriverVersion/chromedriver_win32.exe")
+                                break
+                            case 'osx':
+                                System.setProperty("webdriver.chrome.driver", "../util/drivers/chrome/$chromeDriverVersion/chromedriver_mac32")
+                                break
+                            case 'linux32':
+                                System.setProperty("webdriver.chrome.driver", "../util/drivers/chrome/$chromeDriverVersion/chromedriver_linux32")
+                                break
+                            case 'linux64':
+                                System.setProperty("webdriver.chrome.driver", "../util/drivers/chrome/$chromeDriverVersion/chromedriver_linux64")
+                                break
+                        }
+                        driver = { new ChromeDriver() }
+                        break
+                    case 'phantomjs':
+                        def driverPath
+                        switch (platform) {
+                            case 'windows32':
+                            case 'windows64':
+                                driverPath = '/some/windows/path'
+                                break
+                            case 'osx':
+                                driverPath = '/usr/local/bin/phantomjs'
+                                break
+                            case 'linux32':
+                            case 'linux64':
+                                driverPath = '/usr/bin/phantomjs'
+                                break
+                        }
+                        driver = {
+                            log.trace "Resolved PhantomJS driver path: $driverPath"
+                            def caps = new DesiredCapabilities()
+                            caps.setCapability(
+                                    PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                                    driverPath
+                            )
+                            def d = new PhantomJSDriver(caps)
+                            d.manage().window().setSize(new Dimension(1024,768))
+                            d
+                        }
+                }
+                break
+            case 'remote':
+                driver = {
+                    DesiredCapabilities capabilities
+                    switch (browserType) {
+                        case 'firefox':
+                            capabilities = DesiredCapabilities.firefox()
+                            break
+                        case 'chrome':
+                            capabilities = DesiredCapabilities.chrome()
+                            break
+                        default:
+                            assert ['firefox', 'chrome'].contains(browserType), "Only Firefox and Chrome are currently supported for remote WebDriver sessions."
+                    }
+                    new RemoteWebDriver(
+                            //You must setup your own grid and configure the IP or domain name here
+                            new URL(gridUri), capabilities
+                    )
+                }
+                break
+        }
+        assert driver, "Couldn't configure WebDriver (location:'$browserLocation', type:'$browserType', platform:'$platform')"
+        driver
+    }
+
+    static LinkedHashMap getComputedStyle(JavascriptInterface js, Navigator navigator) {
+        assert navigator.size() == 1, "WebDriver.getComputedStyle() can only be called on Navigators with size() == 1"
+        def map = [:],
+            props
+        if (navigator.@id)
+            props = js."window.getComputedStyle(window.document.getElementById('${navigator.@id}'))"
+        else {
+            log.warn "Fetching computed style via class names; specific results are not guaranteed."
+            props = js."window.getComputedStyle(window.document.getElementsByClassName('${navigator.classes().join("','")}'))"
+        }
+        props.each { map[it] = navigator.css(it) }
+        map
+    }
+
+    static String getElementTextNonRecursively(element) {
+        element.children().inject(element.text()) { text, child ->
+            text - child.text()
+        }.trim()
+    }
+}
